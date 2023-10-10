4c41d9e27e0ab5563b172316bc81c4d30eaccca4
kristen <kristen346@revature.net>
2023-10-10 14:20:46 -0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20 container">
+      <Navbar />
+      <div className='container mx-auto px-12 py-4'>
+        <HeroSection />
+     </div>
+    </main>
+  )
+}

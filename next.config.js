25ed39f45fbf81154bbd4fdb56a4b7577a5bb098
kristen <kristen346@revature.net>
2023-10-10 15:33:25 -0500

    second commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    env: {
+      RESEND_API_KEY:  process.env.RESEND_API_KEY
+    },
+  };
+  
+  module.exports = nextConfig;

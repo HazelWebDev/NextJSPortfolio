bc510fc5b28780f18363f938ae6d4713f3fa3106
Jesse Leonard <jesse1517@gmail.com>
2023-10-10 15:59:21 -0500

    removed email

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,6 +1,5 @@
-import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
@@ -9,11 +8,13 @@ export async function POST() {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ['kristenroggero@gmail.com'],
-      subject: 'Hello world',
-      react: <>
-      <p>Email Body</p>
-      </>
+      to: ["kristenroggero@gmail.com"],
+      subject: "Hello world",
+      react: (
+        <>
+          <p>Email Body</p>
+        </>
+      ),
     });
 
     return NextResponse.json(data);

0f9879accf9165766877e87350c6165426057b7d
Jesse Leonard <jesse1517@gmail.com>
2023-10-11 13:24:00 -0500

    trying to deploy

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,90 +1,93 @@
-"use client"
-import React, {useState, useTransition} from 'react'
-import Image from 'next/image'
-import TabButton from './TabButton'
+"use client";
+import React, { useState, useTransition } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
 
 const tabData = [
-    {
-        title: "Skills",
-        id:"skills",
-        content: (
-            <ul>
-                <li>JavaScript</li>
-                <li>React.JS</li>
-            </ul>
-        )
-    },
-    {
-        title: "Education",
-        id:"education",
-        content: (
-            <ul>
-                <li>California State University, East Bay</li>
-                <li>React.JS</li>
-            </ul>
-        )
-    },
-    {
-        title: "Experience",
-        id:"experience",
-        content: (
-            <ul>
-                <li>Infosys</li>
-                <li>Web Developer</li>
-            </ul>
-        )
-    }
-]
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul>
+        <li>JavaScript</li>
+        <li>React.JS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul>
+        <li>California State University, East Bay</li>
+        <li>React.JS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul>
+        <li>Infosys</li>
+        <li>Web Developer</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
-    const [tab, setTab] = useState("skills")
-    const [isPending, startTransition ] = useTransition();
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
 
-    const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id)
-        });
-    }
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
 
   return (
-    <section className='text-white '>
-        <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <Image src="/AnimeGirl.png" width={500} height={500} />
-        <div className='mt-4 md:mt-0 text-left'>
-            <h2 className='text-4xl font-bold text-pink-400 mb-4'>About Me</h2>
-            <p className='text-base md:text-lg'>
-            I am a person of many passions, the most favored being art and creativity. This is precisely what called me to
-              front-end development: the imaginative synergy between art and technology. The ability to create unique and
-              inspirational websites is fascinating!
-            </p>
-            <div className='flex flex-row mt-8'>
-                <TabButton 
-                selectTab={() => handleTabChange("skills")} 
-                active={tab === "skills"} 
-                >
-                {" "}
-                Skills{" "}
-                </TabButton>
-                <TabButton 
-                selectTab={() => handleTabChange("education")} 
-                active={tab === "education"} 
-                >
-                {" "}
-                Education{" "}
-                </TabButton>
-                <TabButton 
-                selectTab={() => handleTabChange("experience")} 
-                active={tab === "experience"} 
-                >
-                {" "}
-                Experience{" "}
-                </TabButton>
-            </div>
-            <div className='mt-8'>{tabData.find((t) => t.id === tab).content}</div>
+    <section className="text-white ">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/AnimeGirl.png" alt="anime girl" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left">
+          <h2 className="text-4xl font-bold text-pink-400 mb-4">About Me</h2>
+          <p className="text-base md:text-lg">
+            I am a person of many passions, the most favored being art and
+            creativity. This is precisely what called me to front-end
+            development: the imaginative synergy between art and technology. The
+            ability to create unique and inspirational websites is fascinating!
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              {" "}
+              Experience{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {tabData.find((t) => t.id === tab).content}
+          </div>
         </div>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,57 +1,75 @@
-"use client"
-import React, { useState } from 'react'
-import Link from 'next/link'
-import NavLink from './NavLink'
-import {Bars3Icon, XMarkIcon, XmarkIcon } from '@heroicons/react/24/solid'
-import MenuOverlay from './MenuOverlay'
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon, XmarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
+import Image from "next/image";
 
 const navLinks = [
-    {
-        title: "About",
-        path: "#about",
-    },
-    {
-        title: "Projects",
-        path: "#projects",
-    },
-    {
-        title: "Resume",
-        path: "#resume",
-    }
-]
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Resume",
+    path: "#resume",
+  },
+];
 
 const Navbar = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-zinc-600 bg-opacity-90 border-b-4 border-green-400'>
-        <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-4'>
-        <Image className="" src="/FlowerLogo2.svg" width={75} height={75}/>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-zinc-600 bg-opacity-90 border-b-4 border-green-400">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-4">
+        <Image
+          className=""
+          alt="nav bar image"
+          src="/FlowerLogo2.svg"
+          width={75}
+          height={75}
+        />
 
-            <Link href="/" className="text-pink-400 font-semibold "><h3 className='font-bold text-2xl items-center'>Kristen's Portfolio</h3></Link>
-           <div className=' mobile-menu block md:hidden'>
-            {
-                !navbarOpen ? (
-                    <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-green-200 text text-pink-400'><Bars3Icon className='h-5 w-5'/></button>
-                ) : (
-                    <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-green-200 text text-pink-400'><XMarkIcon className='h-5 w-5'/></button>
-                )
-            }
-           </div>
-            <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-                    {navLinks.map((link, index) => (
-                    <li key={index}>
-                        <NavLink href={link.path} title={link.title} />
-                    </li>
-                    ))}
-                </ul>
-            </div>
+        <Link href="/" className="text-pink-400 font-semibold ">
+          <h3 className="font-bold text-2xl items-center">
+            Kristen's Portfolio
+          </h3>
+        </Link>
+        <div className=" mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-green-200 text text-pink-400"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-green-200 text text-pink-400"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
         </div>
-        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

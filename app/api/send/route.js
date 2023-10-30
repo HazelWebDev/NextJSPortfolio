6f279afdf48dc94ef09904aba848b94cc319b826
kristen <kristen346@revature.net>
2023-10-30 16:22:41 -0500

    logo updates

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,6 +5,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
+
 export async function POST() {
   try {
     const data = await resend.emails.send({

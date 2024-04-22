015b23bd9160d9cbef95a4007d115cd20337ef5a
Kristen Roggero <kristenroggero@Kristens-Laptop.attlocal.net>
2024-04-21 21:50:27 -0500

    redo

---
+++ components/Navbar.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const navLinks = [
+  {
+    title: "Resume",
+    path: "/resume",
+  },
+];
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-l1 bg-opacity-90 border-b-4 border-primary">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3">
+        <Image
+          className=""
+          src="/FlowerLogo3.png"
+          alt="FlowerLogo"
+          width={75}
+          height={75}
+        />
+
+        <Link href="/" className="text-dark font-bold ">
+          <h3 className="font-bold text-2xl items-center">
+            Kristen&apos;s Portfolio
+          </h3>
+        </Link>
+        <div className=" mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-l1 text text-primary"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-l1 text text-primary"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 text-dark">
+            <li className="px-10">
+              <Link
+                href={"/resume"}
+                className="block py-2 pl-3 pr-4 text-dark font-semibold sm:text-xl md:p-0 hover:text-primary border-b border-primary"
+              >
+                Resume
+              </Link>
+              <Link
+                href={"/about"}
+                className="block py-2 pl-3 pr-4 text-dark font-semibold sm:text-xl md:p-0 hover:text-primary border-b border-primary"
+              >
+                About Me
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? (
+        <ul className="flex flex-col py-4 items-center">
+          <li>
+            <Link
+              href={"/projects"}
+              className="block py-2 pl-3 pr-4 text-dark font-semibold sm:text-xl md:p-0 hover:text-primary border-b border-primary"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"/resume"}
+              className="block py-2 pl-3 pr-4 text-dark font-semibold sm:text-xl md:p-0 hover:text-primary border-b border-primary"
+            >
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"/about"}
+              className="block py-2 pl-3 pr-4 text-dark font-semibold sm:text-xl md:p-0 hover:text-primary border-b border-primary"
+            >
+              About Me
+            </Link>
+          </li>
+        </ul>
+      ) : null}
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        primary: ["var(--font-agbalumo)"],
+        secondary: ["var(--font-dosis)"],
+      },
+      colors: {
+        primary: "#f472b6", //pink400
+        secondary: "#4ade80", //green400
+        l1: "#bbf7d0", //green200
+        l2: "#fbcfe8", //pink200
+        white: "#f3f4f6", //gray100
+        dark: "#111827", // gray900
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};

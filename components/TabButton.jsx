47f95595dab321d873b4ee053b56ee137714f96b
Kristen <kristenkristen05@gmail.com>
2024-08-01 22:52:24 -0500

    button changes, again

--- components/TabButton.jsx
+++ components/TabButton.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const TabButton = ({ active, selectTab, children }) => {
   const buttonClasses = active
-    ? "p-3 rounded-full border-2 border-white border-2 border-white bg-gradient-to-br from-primary to-secondary text-dark hover:border-white hover:border-3 font-semibold"
+    ? "p-3 rounded-full border-2 border-white border-2 border-white bg-gradient-to-br from-primary to-secondary text-dark hover:border-white font-semibold"
     : "text-white";
   return (
     <button onClick={selectTab}>
       <p
-        className={`mr-3 p-3 hover:border rounded-full shadow-md border-2 border-white hover:bg-gradient-to-br from-primary to-secondary text-dark hover:border-white hover:border-3 font-semibold  ${buttonClasses}`}
+        className={`mr-3 p-3 hover:border rounded-full shadow-md border-2 border-white hover:bg-gradient-to-br from-primary to-secondary text-dark hover:border-white hover:text-dark font-semibold  ${buttonClasses}`}
       >
         {children}
       </p>
